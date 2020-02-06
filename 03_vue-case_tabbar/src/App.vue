5453a170437fb32932bafc64bb0f9587a5dcf54e
0xMilk <1144502582@qq.com>
2020-02-06 19:37:26 +0800

    抽取tarbar的组件,在slot外层包一个div元素 加入路由,进行业务分类,路由与四个模块结合，进行跳转 利用props给子组件传递值，在通过计算属性，动态绑定元素的style

--- 03_vue-case_tabbar/src/App.vue
+++ 03_vue-case_tabbar/src/App.vue
@@ -2,22 +2,23 @@
   <div id="app">
     <router-view></router-view>
     <tab-bar>
-      <tab-bar-item link="/home" activeColor="blue">
+<!--      link与activeColor都是传递给子组件的数据-->
+      <tab-bar-item link="/home" activeColor="">
         <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-name">首页</div>
       </tab-bar-item>
-      <tab-bar-item link="/category" activeColor="blue">
+      <tab-bar-item link="/category" activeColor="">
         <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-name">分类</div>
       </tab-bar-item>
-      <tab-bar-item link="/car" activeColor="blue">
+      <tab-bar-item link="/car" activeColor="">
         <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-name">购物车</div>
       </tab-bar-item>
-      <tab-bar-item link="/profile" activeColor="blue">
+      <tab-bar-item link="/profile" activeColor="">
         <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-name">我的</div>
