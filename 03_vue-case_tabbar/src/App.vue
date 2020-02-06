078800d5835832d8774f92ad53e51ed49c310e99
0xMilk <1144502582@qq.com>
2020-02-06 17:38:14 +0800

    Vue项目练习例子

--- 03_vue-case_tabbar/src/App.vue
+++ 03_vue-case_tabbar/src/App.vue
@@ -1,37 +1,28 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <tab-bar>
+      <TabBarItem></TabBarItem>
+      <TabBarItem></TabBarItem>
+      <TabBarItem></TabBarItem>
+      <TabBarItem></TabBarItem>
+    </tab-bar>
   </div>
 </template>
 
 <script>
+  import TabBar from "./components/tabbar/TabBar";
+  import TabBarItem from "./components/tabbar/TabBarItem";
+
   export default {
     name: 'App',
     components: {
+      TabBar,
+      TabBarItem
     }
   }
 </script>
 
 <style>
   @import "./assets/css/base.css";
-  #tab-bar {
-    display: flex;
-    background-color: #f6f6f6;
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
 
-    box-shadow: 0 -1px 1px rgba(100, 100, 100, .2);
-  }
-  .tab-bar-item{
-    flex: 1;
-    text-align: center;
-    height: 49px;
-  }
 </style>
---
+++ 03_vue-case_tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tab-bar-item">
+    <img src="../../assets/img/tabbar/home.svg" alt="">
+    <div>首页</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem"
+  }
+</script>
+
+<style scoped>
+
+  .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 4px;
+    vertical-align: middle;
+  }
+</style>
