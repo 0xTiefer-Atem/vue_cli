dfa015f524c70368388aceaff2ae4a5879898a76
0xMilk <1144502582@qq.com>
2020-02-06 18:33:10 +0800

    抽取tarbar的组件,在slot外层包一个div元素 加入路由,进行业务分类

--- 03_vue-case_tabbar/src/main.js
+++ 03_vue-case_tabbar/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  VueRouter,
   render: h => h(App)
-})
+});
 
 // require("./assets/css/base.css")
---
+++ 03_vue-case_tabbar/src/router/index.js
@@ -0,0 +1,19 @@
+//0.引入插件
+import Vue from "vue"
+import VueRouter from "vue-router"
+
+//1. 加载VueRouter插件
+Vue.use(VueRouter);
+
+//2. 创建路由对象
+const routes = [
+
+];
+const router = new VueRouter({
+  routes
+});
+
+//3. 导出
+export default router
+
+//4. 去main.js引入
