ec2a1d4456c5189dd48dc8e41e32afd5211143ac
0xMilk <1144502582@qq.com>
2020-02-08 16:43:26 +0800

    axios发送简单的网络请求

--- vue-learn_vue_cli/06_vue-learn-axios/src/main.js
+++ vue-learn_vue_cli/06_vue-learn-axios/src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+//1. 导入axios框架
+import axios from 'axios'
+
+//2. 使用axios
+axios({
+  url: 'http://123.207.32.32:8000/home/multdata'
+}).then(res => {
+  console.log(res);
+})
 
 Vue.config.productionTip = false
 
