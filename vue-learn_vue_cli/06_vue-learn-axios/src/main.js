3eb6ffcbc73a255a9b8bf2b9f3cd394b88644256
0xMilk <1144502582@qq.com>
2020-02-08 16:55:52 +0800

    axios发送get的请求一些方法

--- vue-learn_vue_cli/06_vue-learn-axios/src/main.js
+++ vue-learn_vue_cli/06_vue-learn-axios/src/main.js
@@ -4,11 +4,37 @@ import App from './App'
 import axios from 'axios'
 
 //2. 使用axios
+
+//简单get方法
+// axios({
+//   url: 'http://123.207.32.32:8000/home/multdata',
+//   method: 'get'
+// }).then(res => {
+//   console.log(res);
+// });
+
+//含有参数的get方法(1)
+// axios({
+//   url: 'http://123.207.32.32:8000/home/multdata?k1=v1&k2=v2',
+//   method: 'get'
+// }).then(res => {
+//   console.log(res);
+// });
+
+//含有参数的get方法(2) get请求参数拼接
 axios({
-  url: 'http://123.207.32.32:8000/home/multdata'
+  url: 'http://123.207.32.32:8000/home/multdata',
+  method: 'get',
+  params: {
+    key1: 'value1',
+    key2: 'value2'
+
+  }
 }).then(res => {
   console.log(res);
-})
+});
+
+
 
 Vue.config.productionTip = false
 
