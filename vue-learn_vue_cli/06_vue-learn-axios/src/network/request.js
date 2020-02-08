a8c9b01689f25c42c625fc9531c76592c4cbc7c1
0xMilk <1144502582@qq.com>
2020-02-08 19:58:28 +0800

    对axios网络请求拦截器

--- vue-learn_vue_cli/06_vue-learn-axios/src/network/request.js
+++ vue-learn_vue_cli/06_vue-learn-axios/src/network/request.js
@@ -26,6 +26,25 @@ export function request(config) {
     baseURL: 'url',
     timeout: 5000
   });
+
+  //axios的请求拦截器 两个回调函数
+  instance.interceptors.request.use( config => {
+    //处理业务员
+
+    //将配置返回继续请求
+    return config;
+  }, err => {
+
+  });
+
+  //axios的响应拦截器
+  instance.interceptors.response.use(res => {
+    return res
+  }, error => {
+
+  });
+
+
   //2. 发送网络请求,instance本身就是一个Promise类型可以直接返回
   return instance(config)
 }
