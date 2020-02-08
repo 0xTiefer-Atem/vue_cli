09252db95efee44fbbbb552d9d24024dd591ce77
0xMilk <1144502582@qq.com>
2020-02-08 17:18:22 +0800

    axios中的默认全局配置

--- vue-learn_vue_cli/06_vue-learn-axios/src/main.js
+++ vue-learn_vue_cli/06_vue-learn-axios/src/main.js
@@ -5,6 +5,15 @@ import axios from 'axios'
 
 //2. 使用axios
 
+//axios全局默认配置,举例
+//baseURL,之后的url直接拼资源路径就行了
+axios.defaults.baseURL = "http://47.107.64.157";
+
+//默认已毫秒为单位
+axios.defaults.timeout = 5000
+
+
+
 //简单get方法
 // axios({
 //   url: 'http://123.207.32.32:8000/home/multdata',
@@ -35,13 +44,22 @@ import axios from 'axios'
 // });
 
 //post方法
-axios({
-  url: 'http://123.207.32.32:8000/home/multdata?k1=v1&k2=v2',
-  method: 'post',
-  data: 'json格式'
-}).then(res => {
-  console.log(res);
-});
+// axios({
+//   url: 'http://123.207.32.32:8000/home/multdata?k1=v1&k2=v2',
+//   method: 'post',
+//   data: 'json格式'
+// }).then(res => {
+//   console.log(res);
+// });
+
+//axios同时发送几个并发请求
+// axios.all([axios({
+//   url: 'url1'
+// }), axios({
+//   url: 'url2'
+// })]).then(results => {
+//   console.log(results);
+// });
 
 
 Vue.config.productionTip = false
