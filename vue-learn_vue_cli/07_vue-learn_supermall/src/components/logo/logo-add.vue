83cde5a9f02aa1aee68aafe4efadb507c14e6333
0xMilk <1144502582@qq.com>
2020-03-10 10:38:09 +0800

    测试

--- vue-learn_vue_cli/07_vue-learn_supermall/src/components/logo/logo-add.vue
+++ vue-learn_vue_cli/07_vue-learn_supermall/src/components/logo/logo-add.vue
@@ -1,74 +1,76 @@
 <template>
+  <div>
+    <!--  添加比赛信息的对话框-->
+    <el-dialog :visible.sync="dialogFormVisible" :close-on-click-modal="false" :close-on-press-escape="false">
+      <el-form :model="dataForm" style="text-align:center">
+        <el-form-item label="比赛选择">
+          <!--        div用来显示选择成功后的信息-->
+          <div v-if="isSelect">
 
-<!--  添加比赛信息的对话框-->
-  <el-dialog :visible.sync="dialogFormVisible" :close-on-click-modal="false" :close-on-press-escape="false">
-    <el-form :model="dataForm" style="text-align:center">
-      <el-form-item label="比赛选择">
-<!--        div用来显示选择成功后的信息-->
-        <div v-if="isSelect">
+          </div>
+          <el-button @click="selectMatch()">选择比赛</el-button>
+        </el-form-item>
+        <el-form-item label="配置文案">
+          <el-input v-model="dataForm.info" placeholder="比赛文案"></el-input>
+        </el-form-item>
+        <el-form-item label="显示时间">
+          <el-date-picker
+                  v-model="dataForm.showTime"
+                  type="datetimerange"
+                  range-separator="至"
+                  start-placeholder="开始日期"
+                  end-placeholder="结束日期">
+          </el-date-picker>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer" style="display:flex;justify-content:space-around;">
+        <el-button type="primary" @click="addNewLogo()">添 加</el-button>
+      </div>
+    </el-dialog>
 
-        </div>
-        <el-button @click="selectMatch()">选择比赛</el-button>
-      </el-form-item>
-      <el-form-item label="配置文案">
-        <el-input v-model="dataForm.info" placeholder="比赛文案"></el-input>
-      </el-form-item>
-      <el-form-item label="显示时间">
-        <el-date-picker
-                v-model="dataForm.showTime"
-                type="datetimerange"
-                range-separator="至"
-                start-placeholder="开始日期"
-                end-placeholder="结束日期">
-        </el-date-picker>
-      </el-form-item>
-    </el-form>
-    <div slot="footer" class="dialog-footer" style="display:flex;justify-content:space-around;">
-      <el-button type="primary" @click="addNewLogo()">添 加</el-button>
-    </div>
-  </el-dialog>
+    <!--  选择比赛的对话框-->
+    <el-dialog :visible.sync="dialogFormVisible" :close-on-click-modal="false" :close-on-press-escape="false">
+      <el-form :model="selectMatchForm" style="text-align:center">
 
-<!--  选择比赛的对话框-->
-  <el-dialog :visible.sync="dialogFormVisible" :close-on-click-modal="false" :close-on-press-escape="false">
-    <el-form :model="selectMatchForm" style="text-align:center">
+        <!--      用的el-ui里的布局-->
+        <el-row type="flex" justify="space-around">
+          <el-col :span="6">
+            <el-select v-model="selectMatchForm.sportValue" placeholder="请选择类型">
+              <el-option
+                      v-for="( item, index ) in sportsList"
+                      :key="index"
+                      :label="item.label"
+                      :value="item.value">
+              </el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="比赛时间区间">
+              <el-date-picker
+                      v-model="selectMatchForm.selectTime"
+                      type="datetimerange"
+                      range-separator="至"
+                      start-placeholder="开始日期"
+                      end-placeholder="结束日期">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-input placeholder="赛事名/球队名/赛事ID" v-model="selectMatchForm.findInfo"></el-input>
+          </el-col>
+        </el-row>
 
-<!--      用的el-ui里的布局-->
-      <el-row type="flex" justify="space-around">
-        <el-col :span="6">
-          <el-select v-model="selectMatchForm.sportValue" placeholder="请选择类型">
-            <el-option
-                    v-for="( item, index ) in sportsList"
-                    :key="index"
-                    :label="item.label"
-                    :value="item.value">
-            </el-option>
-          </el-select>
-        </el-col>
-        <el-col :span="6">
-          <el-form-item label="比赛时间区间">
-            <el-date-picker
-                    v-model="selectMatchForm.selectTime"
-                    type="datetimerange"
-                    range-separator="至"
-                    start-placeholder="开始日期"
-                    end-placeholder="结束日期">
-            </el-date-picker>
-          </el-form-item>
-        </el-col>
-        <el-col :span="6">
-          <el-input placeholder="赛事名/球队名/赛事ID" v-model="selectMatchForm.findInfo"></el-input>
-        </el-col>
-      </el-row>
+        <el-table>
 
-      <el-table>
+        </el-table>
+      </el-form>
+      <div slot="footer" class="dialog-footer" style="display:flex;justify-content:space-around;">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="deleteData()">确 定</el-button>
+      </div>
+    </el-dialog>
 
-      </el-table>
-    </el-form>
-    <div slot="footer" class="dialog-footer" style="display:flex;justify-content:space-around;">
-      <el-button @click="dialogFormVisible = false">取 消</el-button>
-      <el-button type="primary" @click="deleteData()">确 定</el-button>
-    </div>
-  </el-dialog>
+  </div>
 
 </template>
 
--- vue-learn_vue_cli/07_vue-learn_supermall/src/components/logo/logo.vue
+++ vue-learn_vue_cli/07_vue-learn_supermall/src/components/logo/logo.vue
@@ -1,71 +1,73 @@
 <template>
-  <el-card>
-<!--    标题和添加按钮-->
-    <div>
-      <span>底部logo配置</span>
-      <el-button @click="NewLogo()">添加</el-button>
-    </div>
+  <div>
+    <el-card>
+      <!--    标题和添加按钮-->
+      <div>
+        <span>底部logo配置</span>
+        <el-button @click="NewLogo()">添加</el-button>
+      </div>
 
-<!--    配置底部logo的表格-->
-    <div class="mod-sys-params">
-      <el-table
-              :v-loading="dataListLoading"
-              :data="dataList"
-              style="width: 100%">
-        <el-table-column
-                type="index"
-                label="序号"
-                width="50">
-        </el-table-column>
-        <el-table-column
-                prop="startTime"
-                label="比赛开始时间"
-                width="180">
-        </el-table-column>
-        <el-table-column
-                prop="clogo"
-                label="赛事logo(底部logo样式)"
-                width="180">
-          <!--插入图片链接的代码-->
-          <template slot-scope="scope">
-            <img  :src="scope.row.clogo" alt="" style="width: 50px;height: 50px">
-          </template>
-        </el-table-column>
-        <el-table-column
-                prop="cname"
-                label="赛事信息">
-        </el-table-column>
-        <el-table-column
-                prop=""
-                label="比赛对阵">
-          <template slot-scope="scope">
-            {{scope.row.homeName}} VS {{scope.row.awayName}}
-          </template>
-        </el-table-column>
-        <el-table-column
-                prop="info"
-                label="配置文案">
-        </el-table-column>
-        <el-table-column
-                prop=""
-                label="显示时间">
-          <template slot-scope="scope">
-            {{scope.row.startTime}}至{{scope.row.endTime}}
-          </template>
-        </el-table-column>
-        <el-table-column
-                prop=""
-                label="操作">
-          <template slot-scope="scope">
-            <el-button @click="EditLogo()">编辑</el-button>
-            <el-button @click="DeleteInfo(scope.$index , scope.row)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <LogoAdd v-if="addDialog" ref="addLogo" @refreshDataList="getDataList"></LogoAdd>
-      <LogoEdit v-if="editDialog" ref="editLogo" @refreshDataList="getDataList"></LogoEdit>
-    </div>
-  </el-card>
+      <!--    配置底部logo的表格-->
+      <div class="mod-sys-params">
+        <el-table
+                :v-loading="dataListLoading"
+                :data="dataList"
+                style="width: 100%">
+          <el-table-column
+                  type="index"
+                  label="序号"
+                  width="50">
+          </el-table-column>
+          <el-table-column
+                  prop="startTime"
+                  label="比赛开始时间"
+                  width="180">
+          </el-table-column>
+          <el-table-column
+                  prop="clogo"
+                  label="赛事logo(底部logo样式)"
+                  width="180">
+            <!--插入图片链接的代码-->
+            <template slot-scope="scope">
+              <img  :src="scope.row.clogo" alt="" style="width: 50px;height: 50px">
+            </template>
+          </el-table-column>
+          <el-table-column
+                  prop="cname"
+                  label="赛事信息">
+          </el-table-column>
+          <el-table-column
+                  prop=""
+                  label="比赛对阵">
+            <template slot-scope="scope">
+              {{scope.row.homeName}} VS {{scope.row.awayName}}
+            </template>
+          </el-table-column>
+          <el-table-column
+                  prop="info"
+                  label="配置文案">
+          </el-table-column>
+          <el-table-column
+                  prop=""
+                  label="显示时间">
+            <template slot-scope="scope">
+              {{scope.row.startTime}}至{{scope.row.endTime}}
+            </template>
+          </el-table-column>
+          <el-table-column
+                  prop=""
+                  label="操作">
+            <template slot-scope="scope">
+              <el-button @click="EditLogo()">编辑</el-button>
+              <el-button @click="DeleteInfo(scope.$index , scope.row)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <LogoAdd v-if="addDialog" ref="addLogo" @refreshDataList="getDataList"></LogoAdd>
+        <LogoEdit v-if="editDialog" ref="editLogo" @refreshDataList="getDataList"></LogoEdit>
+      </div>
+    </el-card>
+  </div>
 </template>
 <script>
   import LogoAdd from'./logo-add'
